f318649957ba0c23295dc9a2147c22dc85fd740c
Furkan Salduz <f.salduz12@gmail.com>
2024-07-28 23:17:45 +0300

    eksik DTO dosyasını ekledim

---
+++ src/main/java/com/sekerbank/bankingapp/Dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.sekerbank.bankingapp.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginRequest {
+    private String username;
+    private String password;
+
+    // Getters and Setters
+
+}
